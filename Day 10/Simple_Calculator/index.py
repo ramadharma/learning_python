b2da4d9a874be702388f5b19a64bb13c97825aa7
rama.dharmaa@gmail.com <rama.dharmaa@gmail.com>
2025-01-01 12:32:17 +0700

    updates calculator day 10

--- Day 10/Simple_Calculator/index.py
+++ Day 10/Simple_Calculator/index.py
@@ -1,9 +1,9 @@
 import art
 import os
 
-# Greetings
-print(art.calculator)
-print("Welcome to Simple Calculator Program!")
+# Function to delete the screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # Adding function
 def add(n1, n2):
@@ -29,6 +29,39 @@ operations = {
     "/": divide,
 }
 
-# how to called the function with dictionary
-test = operations["*"](4, 8)
+# program function
+def calculator():
 
+    # Greetings
+    print(art.calculator)
+    print("Welcome to Simple Calculator Program!")
+
+    should_acc = True
+    num1 = float(input("What is the first number?: ")) # first number inputed
+
+    # program looping
+    while should_acc: 
+
+        # printing the list of operations
+        for symbols in operations:
+            print(symbols)
+
+        operation_symbol = input("Pick an operation?: ") # pick the operation
+        num2 = float(input("What is the next number?: ")) # input the next number
+
+        ans = operations[operation_symbol](num1, num2) # collect the number and operate
+
+        print(f"{num1} {operation_symbol} {num2} = {ans}") # print the output
+
+        choice = input(f"Type 'y' to continue calculating with {ans}, or type 'n' to start a new calculation: ").lower() # selector to counting again or no
+
+        # logic if user type y, the program will looping again
+        if choice == "y":
+            num1 = ans # collect the ans(output) to num1
+
+        else:
+            should_acc = False
+            clear_screen()
+            calculator() # start again the function. it's called recursion
+
+calculator()
